5ebd84b93b2a9c61124a207819ec03b88e8db502
JoshuaCousins <st10081893@vcconnect.edu.za>
2023-05-13 11:57:24 +0200

    Added Additional Icons, Started work on functionality of Add activity page. (Josh)
    Signed-off-by: JoshuaCousins <st10081893@vcconnect.edu.za>

--- app/src/main/java/com/example/opsc7311_part2/AddActivity.kt
+++ app/src/main/java/com/example/opsc7311_part2/AddActivity.kt
@@ -1,11 +1,36 @@
 package com.example.opsc7311_part2
 
-import androidx.appcompat.app.AppCompatActivity
+//Add necessary references for view
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.view.View
+import android.widget.CalendarView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.set
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_activity)
+
+        //Get a reference to the view that we want to access
+        //
+        //Start Date for both text box and editable text
+        val  tilStartDate: TextInputLayout = findViewById(R.id.til_StartDate)
+        val tilStartDate2: TextInputEditText = findViewById(R.id.txtStartDate)
+
+        //Manipulate the view
+        //Adding event handlers to check when the end icon is clicked
+        fun performActionOnClick() {
+
+        }
+
+        tilStartDate.setEndIconOnClickListener {
+            // Code to be executed when the end icon is clicked
+            performActionOnClick()
+        }
+
     }
 }
