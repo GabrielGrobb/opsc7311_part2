fae6b03e9506dde41f85a01bf1b6d4481c518b62
JoshuaCousins <st10081893@vcconnect.edu.za>
2023-05-14 19:11:12 +0200

    Finished Add Activity page (Maybe swap out text boxes for input-specifc views?) Updated AppName in strings.xml to "Colossal Tracker" Started work on Add Activity Page.
    Signed-off-by: JoshuaCousins <st10081893@vcconnect.edu.za>

--- app/src/main/java/com/example/opsc7311_part2/AddActivity.kt
+++ app/src/main/java/com/example/opsc7311_part2/AddActivity.kt
@@ -2,6 +2,7 @@ package com.example.opsc7311_part2
 
 //Add necessary references for view
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -11,23 +12,43 @@ class AddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_activity)
 
-        //Get a reference to the view that we want to access
-        //
-        //Start Date for both text box and editable text
-        val  tilStartDate: TextInputLayout = findViewById(R.id.til_StartDate)
+        //Views
+        val backButton: ImageView = findViewById(R.id.til_BackButton)
+        val tilLocation: TextInputLayout = findViewById(R.id.til_Location)
+        val txtLocation: TextInputEditText = findViewById(R.id.txtLocation)
+        val tilCategory: TextInputLayout = findViewById(R.id.til_Category)
+        val txtCategory: TextInputEditText = findViewById(R.id.txtCategory)
+        val tilStartDate: TextInputLayout = findViewById(R.id.til_StartDate)
         val txtStartDate: TextInputEditText = findViewById(R.id.txtStartDate)
+        val tilEndDate: TextInputLayout = findViewById(R.id.til_EndDate)
+        val txtEndDate: TextInputEditText = findViewById(R.id.txtEndDate)
 
 
-
-        //Manipulate the view
-        //Adding event handlers to check when the end icon is clicked
+        //Functions
         fun performActionOnClick() {
+
+        }
+
+        //Listeners
+        backButton.setOnClickListener(){
+
+        }
+
+        tilLocation.setEndIconOnClickListener(){
+
+        }
+
+        tilCategory.setEndIconOnClickListener(){
 
         }
 
         tilStartDate.setEndIconOnClickListener {
             // Code to be executed when the end icon is clicked
             performActionOnClick()
+        }
+
+        tilEndDate.setEndIconOnClickListener(){
+
         }
 
     }
