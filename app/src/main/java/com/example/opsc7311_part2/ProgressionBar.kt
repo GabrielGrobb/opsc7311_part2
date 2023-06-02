6eba1159a44c4895b5f115ad04ca6374fdf840ef
KeeanByrne <keeanbyrne00@gmail.com>
2023-06-02 13:53:44 +0200

    Completed Layout activity_achievements Page (Keean)
    Added activity_achievements.xml & Schedule class

--- app/src/main/java/com/example/opsc7311_part2/ProgressionBar.kt
+++ app/src/main/java/com/example/opsc7311_part2/ProgressionBar.kt
@@ -1,5 +1,6 @@
 package com.example.opsc7311_part2
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.ProgressBar
@@ -15,22 +16,22 @@ class ProgressionBar : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_achievements)
 
+        /* progressBar.max = 100
+        val currentProgress =
+        ObjectAnimator.ofInt(AchievementsProgressBar, "") */
+
         progressBar = findViewById(R.id.AchievementsProgressBar)
         progressText = findViewById(R.id.achievementsProgressText)
 
         updateProgress(50) // Set initial progress value
 
-        // Call this method to update the progress dynamically
-        // You can call it from any event or process in your code
-        // and update the progress and text accordingly
-        // For example: updateProgress(75)
+
     }
 
     private fun updateProgress(progress: Int) {
         progressBar.progress = progress
         progressText.text = "$progress%"
 
-        // You can also format the progress text as desired
-        // For example: progressText.text = getString(R.string.progress_format, progress)
+
     }
 }
--- app/src/main/java/com/example/opsc7311_part2/Schedule.kt
+++ app/src/main/java/com/example/opsc7311_part2/Schedule.kt
@@ -1,13 +1,14 @@
 package com.example.opsc7311_part2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class AchievementActivity : AppCompatActivity() {
+class Schedule : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_achievements)
-
+        setContentView(R.layout.activity_schedule)
 
     }
+
 }
