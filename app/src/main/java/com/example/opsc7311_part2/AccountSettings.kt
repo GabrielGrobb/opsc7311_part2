00950dfe1787da1dd04ea2c36b9acb9b07cda989
Gabriel Grobbelaar <gabrielgrobbelaar1703@gmail.com>
2023-06-02 14:14:32 +0200

    Account Settings Layout completed. Still need to add full navigation.

--- app/src/main/java/com/example/opsc7311_part2/AccountSettings.kt
+++ app/src/main/java/com/example/opsc7311_part2/AccountSettings.kt
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import com.example.opsc7311_part2.databinding.ActivityAccountSettingsBinding
 import com.example.opsc7311_part2.databinding.ActivityCaptureTaskBinding
-import com.example.opsc7311_part2.databinding.ActivityHomePageBinding
 import com.google.android.material.navigation.NavigationView
 
-class HomePage : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener
+class AccountSettings : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener
 {
-    private lateinit var binding: ActivityHomePageBinding
+    private lateinit var binding: ActivityAccountSettingsBinding
+
+    //............................................................................................//
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        binding = ActivityAccountSettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.navToolbar)
@@ -35,6 +40,31 @@ class HomePage : AppCompatActivity(), View.OnClickListener, NavigationView.OnNav
         binding.navView.bringToFront()
         binding.navView.setNavigationItemSelectedListener(this)
 
+        val minSpinner: Spinner = findViewById(R.id.min_time)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.min_time_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            minSpinner.adapter = adapter
+        }
+
+        val maxSpinner: Spinner = findViewById(R.id.max_time)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.max_time_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            maxSpinner.adapter = adapter
+        }
     }
 
     //............................................................................................//
@@ -44,11 +74,10 @@ class HomePage : AppCompatActivity(), View.OnClickListener, NavigationView.OnNav
 
         when(item.itemId)
         {
-            R.id.nav_account -> {
-                val intent = Intent(applicationContext, AccountSettings::class.java)
+            R.id.nav_home -> {
+                val intent = Intent(applicationContext, HomePage::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 startActivity(intent)
-
             }
 
             R.id.nav_logout -> {
@@ -86,7 +115,5 @@ class HomePage : AppCompatActivity(), View.OnClickListener, NavigationView.OnNav
     }
 
     //............................................................................................//
-
-
 }
 //.........................................EndOfFile..............................................//
