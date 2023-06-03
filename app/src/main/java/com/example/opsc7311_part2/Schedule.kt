8eb58667a8f3bab9e38217b232e2ffe1c7532eea
KeeanByrne <keeanbyrne00@gmail.com>
2023-06-03 13:08:54 +0200

    Added to Schedule.kt (Keean)

--- app/src/main/java/com/example/opsc7311_part2/Schedule.kt
+++ app/src/main/java/com/example/opsc7311_part2/Schedule.kt
@@ -1,108 +1,37 @@
 package com.example.opsc7311_part2
 
-import android.content.Intent
-import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import com.example.opsc7311_part2.databinding.ActivityCaptureTaskBinding
-import com.example.opsc7311_part2.databinding.ActivityScheduleBinding
-import com.google.android.material.navigation.NavigationView
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Spinner
+import android.widget.TextView
 
-class Schedule : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
-
-    private lateinit var binding: ActivityScheduleBinding
-
-    //............................................................................................//
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class Schedule : AppCompatActivity() {
+    private lateinit var menuButton: ImageView
+    private lateinit var schedulePageTitle: TextView
+    private lateinit var currentDate: TextView
+    private lateinit var completedTasks: TextView
+    private lateinit var spinnerSchedule: Spinner
+    private lateinit var dateToday: TextView
+    private lateinit var dateTomorrow: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityScheduleBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_schedule)
+        menuButton = findViewById(R.id.IV_menubutton)
+        schedulePageTitle = findViewById(R.id.txt_SchedulePageTitle)
+        currentDate = findViewById(R.id.CurrentDate)
+        completedTasks = findViewById(R.id.txtCompletedTasks)
+        spinnerSchedule = findViewById(R.id.spinnerSchedule)
+        dateToday = findViewById(R.id.DateToday)
+        dateTomorrow = findViewById(R.id.DateTomorrow)
 
-        setSupportActionBar(binding.navToolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-
-        var toggleOnOff = ActionBarDrawerToggle(this,
-            binding.drawerLayout, binding.navToolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close)
-
-        binding.drawerLayout.addDrawerListener(toggleOnOff)
-        toggleOnOff.syncState()
-
-        binding.navView.bringToFront()
-        binding.navView.setNavigationItemSelectedListener(this)
-
-    }
-
-    //............................................................................................//
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean
-    {
-
-        when(item.itemId) {
-
-            /// Possible need to alter these intents as the create a new task.
-            /// Data might not be saved while using application.
-            /// Refferring to the objects.
-
-            R.id.nav_home -> {
-                val intent = Intent(applicationContext, HomePageTest::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-            }
-
-            R.id.nav_schedule -> {
-                val intent = Intent(applicationContext, Schedule::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-
-            }
-
-            R.id.nav_account -> {
-                val intent = Intent(applicationContext, AccountSettings::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-            }
-            R.id.nav_logout -> {
-                val intent = Intent(applicationContext, LoginActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-            }
+        // Set click listener for the menu button
+        menuButton.setOnClickListener {
+            // Handle menu button click
         }
-        binding.drawerLayout.closeDrawer(GravityCompat.START)
-        // return true marks the item as selected
-        return true
+
+        // TODO: Add code for handling other UI elements and implementing functionality
+
     }
-
-    //............................................................................................//
-
-    override fun onBackPressed()
-    {
-        //if the drawer is open, close it
-        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START))
-        {
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        }
-        else
-        {
-            //otherwise, let the super class handle it
-            super.onBackPressed()
-        }
-    }
-
-    //............................................................................................//
-
-    override fun onClick(v: View?)
-    {
-        /*TODO("Not yet implemented")*/
-    }
-
-    //............................................................................................//
-
 }
-//.........................................EndOfFile..............................................//
