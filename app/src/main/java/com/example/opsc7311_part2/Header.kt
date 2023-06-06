27e15ed3f6a0cb8d209e5ce301b8a967da7f67ce
KeeanByrne <keeanbyrne00@gmail.com>
2023-06-06 13:44:15 +0200

    Added header.xml And Header.kt

---
+++ app/src/main/java/com/example/opsc7311_part2/Header.kt
@@ -0,0 +1,39 @@
+package com.example.opsc7311_part2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+//-----------------------------------------------------------------------------------//
+
+//To display current logged in Username on the Header
+
+
+class Header : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_header)
+
+        // Retrieve the username from ToolBox
+        val username = ToolBox.AccountManager.getSettingsObject().username
+
+
+        // Set the username to the TextView or show the hint as a placeholder
+        val txtUsername = findViewById<TextView>(R.id.txtUsername)
+
+        txtUsername.text = username
+        if (username.isNullOrEmpty()) {
+            txtUsername.text = null
+            txtUsername.hint = "Username"
+        } else {
+            txtUsername.text = username
+            txtUsername.hint = null
+        }
+    }
+}
+
+
+
+
+//-----------------------------------------------------------------------------------//
