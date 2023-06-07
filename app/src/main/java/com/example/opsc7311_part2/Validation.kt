226b9a974e807b1609abdbfbf8aeee799e6c3f1b
GeoffreyCoding <Geoffrey.huth@gmail.com>
2023-06-07 18:57:43 +0200

    Working validation for add activity_add_activity.xml

--- app/src/main/java/com/example/opsc7311_part2/Validation.kt
+++ app/src/main/java/com/example/opsc7311_part2/Validation.kt
@@ -16,22 +16,16 @@ class Validation {
         return pattern.matches(string)
     }
 
-    //Double validation to only allow whole numbers and decimals
-    fun validationLong(string: String): Boolean {
-        val pattern = Regex("^\\d+$")
-        return pattern.matches(string)
-    }
     // Date validation to check if end date is after start date
     fun isEndDateAfterStartDate(startDate: String, endDate: String): Boolean {
         if (startDate.isEmpty() || endDate.isEmpty() || startDate == " " || endDate == " ") {
             return false
         }
-
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         try {
             val start = dateFormat.parse(startDate)
             val end = dateFormat.parse(endDate)
-            if(end > start){
+            if(end >= start){
                 return true
             }
             return false
@@ -40,6 +34,4 @@ class Validation {
         }
         return false
     }
-
-
 }
